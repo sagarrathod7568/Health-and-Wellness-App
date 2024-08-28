5aef2bf93801153178dc5594410e492f7df546ef
sagarrathod7568 <sagarrathod050@gmail.com>
2024-08-29 04:02:21 +0530

    Navbar ,home, login, signup page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import { useState } from "react";
 import "./App.css";
+import { Navbar } from "./components/Sagar-components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./components/Sagar-components/Home";
+import { About } from "./components/Sagar-components/About";
+import { Profile } from "./components/Sagar-components/Profile";
+import { Login } from "./components/Sagar-components/Login";
+import { Signup } from "./components/Sagar-components/Signup";
+
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Hello guys, let's Begin</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/signup" element={<Signup/>}/>
+      </Routes>
     </>
   );
 }
