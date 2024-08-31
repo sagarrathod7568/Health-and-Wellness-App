bcd970a5bd11f38c7df73bc147b8358c0414e42b
Om Shrestha <omshre06@gmail.com>
2024-08-31 16:50:32 +0530

    front-end update

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,36 @@ import "./App.css";
 import { Navbar } from "./components/Sagar-components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./components/Sagar-components/Home";
-import { About } from "./components/Sagar-components/About";
+import{About} from  "./components/Om-components/components/about"
 import { Profile } from "./components/Sagar-components/Profile";
 import { Login } from "./components/Sagar-components/Login";
 import { Signup } from "./components/Sagar-components/Signup";
 import { ForgotPassword } from "./components/Sagar-components/ForgotPassword";
 import { ResetPassword } from "./components/Sagar-components/ResetPassword";
-import { Article } from "./components/Sagar-components/Articles";
+import{Articles} from "./components/Om-components/components/articles"
+import { LearnMore } from "./components/Om-components/components/learnmore";
+import { ArtContextProvider } from "./components/Om-components/context/artContextProv";
+
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+    <ArtContextProvider>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/articles" element={<Article />} />
+        <Route path="/articles" element={<Articles />} />
         <Route path="/about" element={<About />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/forgotPassword" element={<ForgotPassword />} />
         <Route path="/resetPassword" element={<ResetPassword />} />
+        <Route path="/learn" element={<LearnMore />} />
       </Routes>
+      </ArtContextProvider>
     </>
   );
 }
