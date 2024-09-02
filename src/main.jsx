d7115d76cc659afa0d75804a523f650e32f1629b
sagarrathod7568 <sagarrathod050@gmail.com>
2024-09-02 14:46:25 +0530

    Chakra added

--- src/main.jsx
+++ src/main.jsx
@@ -7,18 +7,21 @@ import { AuthProvider } from "./context/AuthContext.jsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const clientId =
   "860200323517-qrvrupro0j1nrh53iv98ubn2npgog2hl.apps.googleusercontent.com";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <GoogleOAuthProvider clientId={clientId}>
-          <App />
-        </GoogleOAuthProvider>
-      </AuthProvider>
-    </BrowserRouter>
+    <ChakraProvider>
+      <BrowserRouter>
+        <AuthProvider>
+          <GoogleOAuthProvider clientId={clientId}>
+            <App />
+          </GoogleOAuthProvider>
+        </AuthProvider>
+      </BrowserRouter>
+    </ChakraProvider>
   </StrictMode>
 );
